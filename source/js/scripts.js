77835d49639fc9d557a7906848b27203247c9a30
Tim <posseslash@gmail.com>
2019-06-25 17:49:02 +0200

    [style][js] adding slider

---
+++ source/js/scripts.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+  $('#js-index-slick-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+  });
+
+});
