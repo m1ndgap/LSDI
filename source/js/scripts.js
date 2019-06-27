d72e642c69b71fee8d95d956378125b01975d349
Tim <posseslash@gmail.com>
2019-06-27 13:21:13 +0200

    [style][js] gallery with magnific popup

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -7,6 +7,17 @@ $( document ).ready(function() {
   //   dots: true,
   // });
 
-
+  $('#js-index-gallery a').magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true, // set to true to enable gallery
+      preload: [0,2], // read about this option in next Lazy-loading section
+      navigateByImgClick: true,
+      arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+      tPrev: 'Previous (Left arrow key)', // title for left button
+      tNext: 'Next (Right arrow key)', // title for right button
+      tCounter: '<span class="mfp-counter"></span>' // markup of counter
+  }
+});
 
 });
