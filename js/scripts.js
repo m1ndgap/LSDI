87f6c07a220d304645c58b39721d1d63cf47944c
Tim <posseslash@gmail.com>
2019-07-02 14:51:03 +0200

    [style] added masonry for the gallery

--- js/scripts.js
+++ js/scripts.js
@@ -28,12 +28,46 @@ $( document ).ready(function() {
       },
     focusout: function() {
       if ($(this).val() != ""){
-        console.log(12312313);
+
       } else {
         $(this).siblings('.form__label').removeClass('label-shrink');
       }
     }
   });
+
+  $('#js-index-gallery').masonry({
+    columnWidth: '.masonry-sizer',
+    gutter: '.masonry-gutter',
+    itemSelector: '.gallery__image',
+    percentPosition: true
+  })
+
+  $('#js-gallery-add-items').on( 'click', function() {
+  // create new item elements
+  var $items = $('.js-gallery-extra');
+
+  // append items to grid
+  $('#js-index-gallery').append($items)
+    // add and lay out newly appended items
+  .masonry( 'appended', $items );
+
+    $('#js-index-gallery a').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true, // set to true to enable gallery
+        preload: [0,2], // read about this option in next Lazy-loading section
+        navigateByImgClick: true,
+        arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+        tPrev: 'Previous (Left arrow key)', // title for left button
+        tNext: 'Next (Right arrow key)', // title for right button
+        tCounter: '<span class="mfp-counter"></span>' // markup of counter
+      }
+    });
+  $('#js-gallery-add-items').fadeOut(200);
+  $('.js-gallery-extra').removeClass('js-gallery-extra')
+  });
+
+
 
 
 });
